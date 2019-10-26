833cc7df0e7d2606894affe67dc4e3f25547a455
Gabriel Santillan <gabrieladansantillan@hotmail.com>
2019-10-26 11:48:30 -0300

    Project definition

---
+++ LicenseKeyGenerator/Core/LicenseKeyManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace LicenseKeyGenerator.Core
+{
+    internal static class LicenseKeyManager
+    {
+        static byte[] SaltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        static LicenseKeyManager()
+        {
+
+        }
+
+        public static void CreateKeyFile(string productKey, DateTime expireDate, string password)
+        {
+            try
+            {
+                if (expireDate <= DateTime.Now)
+                    throw new Exception("Fecha inválida");
+                if (string.IsNullOrEmpty(productKey))
+                    throw new Exception("Debe ingresar una clave de producto");
+                if (string.IsNullOrEmpty(password))
+                    throw new Exception("Debe ingresar un contraseña");
+                string content = string.Format("PRODUCTKEY={0}\r\nEXPIRYDATE={1}", productKey, expireDate);
+                string encrypt = Encrypt(content, password);
+                byte[] bytes = Encoding.UTF8.GetBytes(encrypt);
+                string path = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "LicenseKey.key";
+                var fileStream = File.Create(path);
+                fileStream.Write(bytes, 0, bytes.Length);
+                fileStream.Flush();
+                fileStream.Close();
+                fileStream.Dispose();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private static string Encrypt(string plainText, string password)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new Exception("No se encontró contenido para encriptar");
+            if (string.IsNullOrEmpty(password))
+                throw new Exception("No se suministró una clave para realizar la encriptación");
+
+            byte[] encryptedBytes = null;
+
+            try
+            {
+                byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(plainText);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (RijndaelManaged AES = new RijndaelManaged())
+                    {
+                        var key = new Rfc2898DeriveBytes(passwordBytes, SaltBytes, 1000);
+
+                        AES.KeySize = 256;
+                        AES.BlockSize = 128;
+                        AES.Key = key.GetBytes(AES.KeySize / 8);
+                        AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                        AES.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                            cs.Close();
+                        }
+
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al Encriptar", e);
+            }
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+    }
+}
---
+++ LicenseKeyGenerator/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LicenseKeyGenerator.Core;
+using System;
+using System.Windows;
+
+namespace LicenseKeyGenerator
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_License_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string productKey = ProductKey.Text;
+                string password = Password.Password;
+                string expiryDateText = ExpiryDate.Text;
+                if (string.IsNullOrEmpty(expiryDateText))
+                    throw new Exception("Debe seleccionar una fecha");
+                DateTime expiryDate = Convert.ToDateTime(expiryDateText);
+                LicenseKeyManager.CreateKeyFile(productKey, expiryDate, password);
+                MessageBox.Show("Archivo de licencia generado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void New_Key_Click(object sender, RoutedEventArgs e)
+        {
+            ProductKey.Text = Guid.NewGuid().ToString().ToUpper();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ProductKey.Text = string.Empty;
+            ExpiryDate.Text = string.Empty;
+            Password.Password = string.Empty;
+        }
+    }
+}
